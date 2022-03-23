5c385672f35b5657320589a2dde63b526182bcd0
RichGriff <rich.griffiths89@gmail.com>
2022-03-23 17:28:56 +0000

    Added Framer Motion

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,28 +1,41 @@
 import React, { useContext } from 'react'
 import { LoginContext } from "./LoginContext";
+import { motion } from 'framer-motion'
 
 const SignIn = () => {
     const { switchToSignUp } = useContext(LoginContext);
 
     return (
-        <form>
-            <section className="copy">
-                <h2>Sign In</h2>
-                <p className="tagline">Let's see what we can help with today.</p>
-            </section>
-            <div className="input-container">
-                <label htmlFor="email">Email</label>
-                <input id="email" name="email" type="email" />
-            </div>
-            <div className="input-container">
-                <label htmlFor="password">Password</label>
-                <input id="password" name="password" type="password" />
-            </div>
-            <button className="signin-btn" type='submit'>Sign In</button>
-            <section className="copy">
-                <p>Dont have an account set up? <a href="#" onClick={switchToSignUp}><strong>Sign Up Here!</strong></a></p>
-            </section>
-        </form>
+        <motion.div
+            initial={{ x: 200, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ type: "spring", stiffness: 100 }}
+        >
+            <form>
+                <section className="copy">
+                    <h2>Sign In</h2>
+                    <p className="tagline">Let's see what we can help with today.</p>
+                </section>
+                <div className="input-container">
+                    <label htmlFor="email">Email</label>
+                    <input id="email" name="email" type="email" />
+                </div>
+                <div className="input-container">
+                    <label htmlFor="password">Password</label>
+                    <input id="password" name="password" type="password" />
+                </div>
+                <motion.button
+                    whileHover={{ scale: 1.05 }}
+                    className="signin-btn" 
+                    type='submit'
+                >
+                    Sign In
+                </motion.button>
+                <section className="copy">
+                    <p>Dont have an account set up? <a href="#" onClick={switchToSignUp}><strong>Sign Up Here!</strong></a></p>
+                </section>
+            </form>
+        </motion.div>
     )
 }
 
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,33 +1,46 @@
 import React, { useContext } from 'react'
 import { LoginContext } from './LoginContext';
+import { motion } from 'framer-motion'
 
 const SignUp = () => {
   const { switchToSignIn } = useContext(LoginContext);
 
   return (
-    <form>
-      <section className="copy">
-        <h2>Sign Up Today!</h2>
-        <p className="tagline">To start managing your tenancy online!</p>
-        <p>Already have an account? <a href="#" onClick={switchToSignIn}><strong>Log In</strong></a></p>
-      </section>
-      <div className="input-container">
-        <label htmlFor="fname">Full Name</label>
-        <input id="fname" name="fname" type="text" />
-      </div>
-      <div className="input-container">
-        <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="email" />
-      </div>
-      <div className="input-container">
-        <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" />
-      </div>
-      <button className="signup-btn" type='submit'>Sign Up</button>
-      <section className="copy legal">
-        <p><span className='small'>By continuing, you agree accept our <br/> <a href="">Privacy Policy</a> &amp; <a href="">Terms of Service</a>.</span></p>
-      </section>
-    </form>
+    <motion.div
+      initial={{ x: 200, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ type: "spring", stiffness: 100 }}
+    >
+      <form>
+        <section className="copy">
+          <h2>Sign Up Today!</h2>
+          <p className="tagline">To start managing your tenancy online!</p>
+          <p>Already have an account? <a href="#" onClick={switchToSignIn}><strong>Log In</strong></a></p>
+        </section>
+        <div className="input-container">
+          <label htmlFor="fname">Full Name</label>
+          <input id="fname" name="fname" type="text" />
+        </div>
+        <div className="input-container">
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" type="email" />
+        </div>
+        <div className="input-container">
+          <label htmlFor="password">Password</label>
+          <input id="password" name="password" type="password" />
+        </div>
+        <motion.button
+            whileHover={{ scale: 1.05 }}
+            className="signup-btn" 
+            type='submit'
+        >
+            Sign Up
+        </motion.button>
+        <section className="copy legal">
+          <p><span className='small'>By continuing, you agree accept our <br/> <a href="">Privacy Policy</a> &amp; <a href="">Terms of Service</a>.</span></p>
+        </section>
+      </form>
+    </motion.div>
   )
 }
 
